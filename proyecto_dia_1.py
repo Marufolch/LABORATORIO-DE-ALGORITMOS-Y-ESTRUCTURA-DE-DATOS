07521a592fa32ecfb9cc97c022b4996d1020d020
Marufolch <marufolch@gmail.com>
2024-09-25 20:12:59 -0300

    Add files via upload

---
+++ proyecto_dia_1.py
@@ -0,0 +1,6 @@
+print("""'a continuacion, te haremos 2 preguntas, 
+que deberas responder con una sola palabra,
+para para formar una frase que le de nombre a tu marca'""")
+palab1= input("ingrese un el nombre de un animal: ")
+palab2 = input("ingresa un color: ")
+nombreM = print(f'un posible nombre para cerveza es {palab1} {palab2}')
